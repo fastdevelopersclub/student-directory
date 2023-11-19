32f58b425fe896e2c28ec2603eaac1acb2ed6634
Mohammad Saeed <msaeedsaeedi@outlook.com>
2023-11-19 12:43:58 +0500

    Routing
    Routes:
    - home
    - login
    - profile
    - results (search)
    
    + Lazy Loading Implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./layout/header/header.component";
-import { FooterComponent } from "./layout/footer/footer.component";
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [
+    CommonModule,
+    RouterModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+  ],
 })
 export class AppComponent {
   title = 'Students-Directory';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./Pages/landing/landing.component').then(
+        (m) => m.LandingComponent
+      ),
+  },
+  {
+    path:'login',
+    loadComponent: () =>
+      import('./Pages/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+  {
+    path:'profile/:username',
+    loadComponent: () =>
+      import('./Pages/profile/profile.component').then(
+        (m) => m.ProfileComponent
+      ),
+  },
+  {
+    path:'results',
+    loadComponent: () =>
+      import('./Pages/search-results/search-results.component').then(
+        (m) => m.SearchResultsComponent
+      ),
+  }
+];
